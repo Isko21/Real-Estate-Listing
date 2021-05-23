b52d8ee96621e4d7e70c74a3d6c9f5ee130e4b9d
Iskhak Suranov <76448222+Isko21@users.noreply.github.com>
2021-05-23 22:51:49 +0100

    Add files via upload

---
+++ BigBanTheory_Project5/Form2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigBanTheory_Project5
+{
+    public partial class Form2 : Form
+    {
+        public static int a=0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        string b;
+        List<string> listID = new List<string>();
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            while (!listID.Contains(b))
+            {
+                Random random = new Random();
+                char a = (char)random.Next('A', 'Z');
+                b = $"{a}-";
+                for (int i = 0; i < 3; i++)
+                {
+                    int c = random.Next(0, 10);
+                    b += c.ToString();
+                }
+                idTXT.Text = b;
+                listID.Add(b);
+            }
+            
+        }
+        OpenFileDialog openFile = new OpenFileDialog();
+        string imagePath3;
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            openFile.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                images.Image = new Bitmap(openFile.FileName);
+                path.Text = openFile.FileName.ToString();
+            }
+            string imagePath = openFile.FileName.ToString();
+            string imagePath1 = imagePath.ToString();
+            string imagePath2 = imagePath1.Substring(imagePath1.LastIndexOf("\\"));
+            imagePath3 = imagePath2.Remove(0, 1);
+        }
+        string info = "";
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int size = Convert.ToInt32(sizeBox.Text);
+                int floor = Convert.ToInt32(floorBox.Text);
+                int age = Convert.ToInt32(ageBox.Text);
+                string address = addressBox.Text;
+                int rooms = Convert.ToInt32(roomBox.Text);
+                int bathrooms = Convert.ToInt32(bathRoomBox.Text);
+                string contract = contractBox.SelectedItem.ToString();
+                string price = priceBox.Text +" "+ currencyBox.SelectedItem.ToString();
+                string ownName = nameBox.Text;
+                string owmLast = lastNameBox.Text;
+                string ownDate = ownerDateBirth.Value.ToString("dd-MMMM-yyyy");
+                string ownNum = numBox.Text;
+                string ownEmail = emailBox.Text;
+                string ownAddress = ownerAddressBox.Text;
+
+                string newPath = b + "-" + imagePath3;
+
+                File.Copy(path.Text, $@"Images\{b + "-" + imagePath3}");
+
+                Property property = new Property(b, size, floor, age, address, rooms, bathrooms,  contract, info.Remove(info.Length - 2, 1), price, newPath);
+                property.Name = ownName; property.LastName = owmLast; property.DateOfBirth = ownDate; 
+                property.Number = ownNum; property.Email = ownEmail; property.OwnerAddress = ownAddress;
+
+                AllProperties.allPropertiesBeforeTxtFile.Add(property);
+                Writer writer = new Writer();
+                writer.WriteItDown();
+                a++;
+                Default();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Missing field", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void checkedChangeForAllCheckBoxes(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            if (checkBox.Checked)
+            {
+                info += checkBox.Text + ", ";
+            }
+        }
+        public void Default()
+        {
+            sizeBox.Text = ""; floorBox.Text = ""; roomBox.Text = ""; bathRoomBox.Text = ""; ageBox.Text = "";
+            addressBox.Text = ""; ownerAddressBox.Text = ""; nameBox.Text = ""; lastNameBox.Text = ""; numBox.Text = "";
+            emailBox.Text = ""; images.Image = Properties.Resources.no_image; priceBox.Text = "";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Hide();
+            form1.Show();
+        }
+    }
+}
---
+++ BigBanTheory_Project5/Form4.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigBanTheory_Project5
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Hide();
+            form1.Show();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {   
+
+        }
+        List<Result> allResults = new List<Result>();
+        int currentPage = 1;
+        int maxPage;
+        int x=0;
+        List<string> vs = File.ReadAllLines(@"Property.txt").ToList();
+        private void search_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+            previous.Enabled = true;
+            next.Enabled = true;
+            navigation.Enabled = true;
+            allResults.Clear();
+            if (radioButton1.Checked)
+            {
+                int min = Convert.ToInt32(textBox1.Text);
+                int max = Convert.ToInt32(textBox2.Text);
+                for(int i = 10; i < vs.Count; i-=-18)
+                {
+                    try
+                    {
+                        int cena = Convert.ToInt32(Reader.LastRemover(Reader.FirstRemover(vs[i])));
+                        if (cena > min && cena < max)
+                        {
+                            Result result = new Result(vs[i - 10], Convert.ToInt32(vs[i - 9]), Convert.ToInt32(vs[i - 8]), Convert.ToInt32(vs[i - 7]), Convert.ToInt32(vs[i - 6]), Convert.ToInt32(vs[i - 5]),
+                                vs[i - 4], vs[i - 3], vs[i - 2], vs[i-1], vs[i], vs[i++], vs[i+2], vs[i +3],
+                                vs[i + 5], vs[i + 6], vs[i + 7]);
+                            allResults.Add(result);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    catch (Exception es)
+                    {
+                        MessageBox.Show(es.Message);
+                    }
+                }
+                ReadIt();
+            }
+            if (radioButton2.Checked)
+            {
+                int min = Convert.ToInt32(textBox3.Text);
+                int max = Convert.ToInt32(textBox4.Text);
+                for (int i = 6; i < vs.Count / 18; i++)
+                {
+                    try
+                    {
+                        int rooms = Convert.ToInt32(Reader.FirstRemover(vs[i]));
+                        if (rooms > min && rooms < max)
+                        {
+                            Result result = new Result(vs[i - 10], Convert.ToInt32(vs[i - 9]), Convert.ToInt32(vs[i - 8]), Convert.ToInt32(vs[i - 7]), Convert.ToInt32(vs[i - 6]), Convert.ToInt32(vs[i - 5]),
+                                vs[i - 4], vs[i - 3], vs[i - 2], vs[i - 1], vs[i], vs[i++], vs[i + 2], vs[i + 3],
+                                vs[i + 5], vs[i + 6], vs[i + 7]);
+                            allResults.Add(result);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    catch (Exception es)
+                    {
+                        MessageBox.Show(es.Message);
+                    }
+                }
+                ReadIt();
+            }
+            if (radioButton3.Checked)
+            {
+                string find = contractBox.SelectedItem.ToString();
+                int i = vs.IndexOf(find);
+                ReadIt();
+            }
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            maxPage = allResults.Count/18;
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            if (currentPage == 1)
+            {
+                previous.Enabled = true;
+            }
+            else
+            {
+                x -= 18;
+                currentPage -= 1;
+            }
+        }
+
+        private void previous_Click(object sender, EventArgs e)
+        {
+            if (currentPage == maxPage)
+            {
+                next.Enabled = true;
+            }
+            else
+            {
+                x += 18;
+                currentPage += 1;
+            }
+        }
+        public void ReadIt()
+        {
+            label20.Text = allResults[x].Id;
+            size.Text = allResults[x].Size.ToString();
+            floor.Text = allResults[x].Floor.ToString();
+            age.Text = allResults[x].Floor.ToString();
+            address.Text = allResults[x].Address;
+            rooms.Text = allResults[x].Rooms.ToString();
+            baths.Text = allResults[x].BathRooms.ToString();
+            pictureBox1.ImageLocation = (@"Images\" + Reader.FirstRemover(allResults[x].Path));
+            contract.Text = allResults[x].Contract;
+            price.Text = allResults[x].Price;
+            name.Text = allResults[x].Name;
+            lastName.Text = allResults[x].LastName;
+            number.Text = allResults[x].Number;
+            email.Text = allResults[x].Email;
+            dateBirth.Text = allResults[x].DateOfBirth;
+            ownerAddres.Text = allResults[x].OwnerAddress;
+        }
+    }
+}
