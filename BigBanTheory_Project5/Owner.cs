0e5501f80b0014c6a530e2a2654a0a3e48741208
Iskhak Suranov <76448222+Isko21@users.noreply.github.com>
2021-05-31 12:53:44 +0100

    Add files via upload

---
+++ BigBanTheory_Project5/Owner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBanTheory_Project5
+{
+    class Owner
+    {
+        string name, lastName, dateOfBirth, number, email, ownerAddress;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public string DateOfBirth { get { return dateOfBirth; } set { dateOfBirth = value; } }
+        public string Number { get { return number; } set { number = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public string OwnerAddress { get { return ownerAddress; } set { ownerAddress = value; } }
+
+    }
+}
---
+++ BigBanTheory_Project5/Writer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BigBanTheory_Project5
+{
+    class Writer
+    {
+        public void WriteItDown()
+        {
+            FileStream fileStream = new FileStream("Property.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+
+            streamWriter.WriteLine("____________________________________");
+            streamWriter.WriteLine($"ID: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].ID}");
+            streamWriter.WriteLine($"Size: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Size}");
+            streamWriter.WriteLine($"Floor: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Floor}");
+            streamWriter.WriteLine($"Age: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Age}");
+            streamWriter.WriteLine($"Address: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Address}");
+            streamWriter.WriteLine($"Rooms: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Rooms}");
+            streamWriter.WriteLine($"Bathrooms: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].BathRooms}");
+            streamWriter.WriteLine($"ContractType: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Contract}");
+            streamWriter.WriteLine($"Options: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Options}");
+            streamWriter.WriteLine($"Price: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Price}");
+            streamWriter.WriteLine($"Image: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Path}");
+
+            streamWriter.WriteLine($"OwnerName: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Name}");
+            streamWriter.WriteLine($"OwnerLastName: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].LastName}");
+            streamWriter.WriteLine($"OwnerDateOfBirth: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].DateOfBirth}");
+            streamWriter.WriteLine($"OwnerPhoneNumber: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Number}");
+            streamWriter.WriteLine($"OwnerEmail: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].Email}");
+            streamWriter.WriteLine($"OwnerAddress: {AllProperties.allPropertiesBeforeTxtFile[Form2.a].OwnerAddress}");
+
+            streamWriter.Close();
+            fileStream.Close();
+            MessageBox.Show("Property added!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+
+}
